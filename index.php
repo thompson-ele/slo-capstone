35d683d453b406dce569f5a7efb2886989bc050f
Elevila Thompson <ele.thompson12@gmail.com>
2017-07-06 17:50:52 -0700

    Added beginning save and delete methods

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
 
     <div ng-controller="mainCtrl" class="list">
         <h1 ng-click="helloWorld()">My TODOs</h1>
-        <div>
+
+        <!-- ng-class adds the .editing-item or .edited classes if the editing or todo.edited variables are TRUE -->
+        <div class="item" ng-class="{'editing-item': editing, 'edited': todo.edited}" ng-repeat="todo in todos">
             <input ng-model="todo.completed" type="checkbox"/>
-            <label ng-hide="editing" class="editing-label" ng-click="helloWorld()">
+            <label ng-hide="editing" ng-click="helloWorld()">
             {{todo.name}}</label>
-            <input ng-show="editing" ng-model="todo.name" class="editing-label" type="text"/>
+            <!-- ng-change sets todo.edited to TRUE if something has changed in this input -->
+            <input ng-change="todo.edited = true" ng-blur="editing = false;" ng-show="editing" ng-model="todo.name" class="editing-label" type="text"/>
 
             <div class="actions">
-                <a href="" class="btn btn-primary" ng-click="editing = !editing"><i class="fa fa-pencil"></i> Edit</a>
-                <a href="" class="btn btn-primary" ng-click="helloWorld()"><i class="fa fa-floppy-o"></i> Save</a>
-                <a href="" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
+                <a href="" ng-click="editing = !editing" class="btn btn-primary"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="" ng-click="saveTodo(todo)" class="btn btn-primary"><i class="fa fa-floppy-o"></i> Save</a>
+                <a href="" ng-click="deleteTodo(todo, $index)" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
             </div>
         </div>
         {{todos}}
-    </div>
+    </div><!--mainCtrl-->
 </div><!--.container-->
 
 <?php include('views/term.php'); ?>
--- js/app.js
+++ js/app.js
@@ -1,19 +1,47 @@
 var todoListApp = angular.module("todoListApp", []);
 
-todoListApp.controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
-    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
-  };
+todoListApp.controller('mainCtrl', function($scope, dataService) {
   
-  $scope.todos = [
-    {"name": "clean the house"}, 
-    {"name": "water the dog"}, 
-    {"name": "feed the lawn"},
-    {"name": "pay dem bills"},
-    {"name": "run"}, 
-    {"name": "swim"}
-  ];
+  $scope.helloConsole = dataService.helloConsole;
 
-  $scope.name = 'ELE';
-  
+  $scope.learningNgChange = function() {
+    console.log("An input changed!");
+  };
+
+  dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data; // Assigning this inside a function prevents the function from running before receiving a response
+  });
+
+  $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1); // Removes the one thing based on the index
+  }
+
+  $scope.saveTodo = function(todo) {
+      dataService.saveTodo(todo);
+  }
+})
+
+.service('dataService', function($http) { // Need to include $http provider to handle HTTP requests
+
+    this.helloConsole = function() {
+        console.log("This is a service woo!");
+    }
+
+    this.getTodos = function(callback) {
+        $http.get('mock/todos.json')
+             .then(callback);
+    }
+
+    this.deleteTodo = function(todo) { // Accepts parameter of todo being deleted
+        console.log("The " + todo.name + " todo has been deleted!");
+        // other logic
+    }
+    
+    this.saveTodo = function(todo) {
+        console.log("The " + todo.name + " todo has been saved!");
+        // other logic
+    }
+
 });
