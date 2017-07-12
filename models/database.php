3cad709249e3ec1b82001b4b91b751c4ae017f14
Elevila Thompson <ele.thompson12@gmail.com>
2017-07-12 12:58:57 -0700

    Updated for Heroku DB

--- models/database.php
+++ models/database.php
@@ -3,12 +3,12 @@
 // https://devcenter.heroku.com/articles/cleardb#using-cleardb-with-php
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$url = parse_url(getenv("mysql://bec7c0d6d3a1d5:e2335f56@us-cdbr-iron-east-03.cleardb.net/heroku_926f28b26fed366?reconnect=true"));
+// $url = parse_url(getenv("mysql://bec7c0d6d3a1d5:e2335f56@us-cdbr-iron-east-03.cleardb.net/heroku_926f28b26fed366?reconnect=true"));
 
-$host       = 'us-cdbr-iron-east-03.cleardb.net'; //$url["host"];
-$username   = 'bec7c0d6d3a1d5'; //$url["user"];
-$password   = 'e2335f56'; //$url["pass"];
-$database   = 'heroku_926f28b26fed366'; //substr($url["path"], 1);
+$host       = $url["host"];//'us-cdbr-iron-east-03.cleardb.net'; 
+$username   = $url["user"];//'bec7c0d6d3a1d5';
+$password   = $url["pass"];//'e2335f56'; 
+$database   =  substr($url["path"], 1);//'heroku_926f28b26fed366';
 
 // Create PDO object
 try {
